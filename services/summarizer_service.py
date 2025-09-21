d7d2aec95c234ffe3b4d015e01116afafefc2b38
Pankaj Rana <pankajrana@Pankajs-MacBook-Pro.local>
2025-09-21 17:47:27 +0530

    add small hugging face model

--- services/summarizer_service.py
+++ services/summarizer_service.py
@@ -8,7 +8,7 @@ load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
 
 # Load model once
-summarizer = pipeline(task="summarization", model="facebook/bart-large-cnn",token=hf_token)
+summarizer = pipeline(task="summarization", model="eenzeenee/t5-small-korean-summarization",token=hf_token)
 
 def generate_summary(text: str) -> str:
     summary = summarizer(text, max_length=50, min_length=25, do_sample=False)
